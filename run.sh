977cdf27488ed7cf6aad1029a632bc9603b7aeef
jasoncao <caozenghuang@gmail.com>
2022-04-04 02:54:51 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #bin/bash
 echo -e "---------docker Login--------"
-sudo docker login --username=$1 ccr.ccs.tencentyun.com --password=$2
+docker login --username=$1 ccr.ccs.tencentyun.com --password=$2
 echo -e "---------docker Stop--------"
 docker stop clocktower
 echo -e "---------docker Rm--------"
 docker rm clocktower
 docker rmi ccr.ccs.tencentyun.com/caozenghuang/clocktower:latest
 echo -e "---------docker Pull--------"
-sudo docker pull ccr.ccs.tencentyun.com/caozenghuang/clocktower:latest
+docker pull ccr.ccs.tencentyun.com/caozenghuang/clocktower:latest
 echo -e "---------docker Create and Start--------"
 docker run -d -p 80:80 --name clocktower --restart=always ccr.ccs.tencentyun.com/caozenghuang/clocktower:latest
 echo -e "---------deploy Success--------"
